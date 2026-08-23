4b2865f7031b6849d3d23e3589037d2e5defb3b2
yoichisugawara <yoichisugawara@gmail.com>
2026-08-24 00:32:17 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'sunshine-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './admin.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
